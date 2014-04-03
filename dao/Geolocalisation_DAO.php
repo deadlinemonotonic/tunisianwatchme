e84cf8b34f8bcc667ac27dfcb74af3acfd44fe06
DELL <DELL@MEKNI_Aymen>
2014-04-03 04:02:40 +0200

    Merge origin/master
    Conflicts:
    	dao/utilisateur_dao.php

--- dao/Geolocalisation_DAO.php
+++ dao/Geolocalisation_DAO.php
@@ -10,13 +10,50 @@ include_once '../connection/connection.php';
 include_once '../entity/GeolocalistionEntity.php';
 
 class GeolocalisationDAO {
-    
 
-    function Insert($lon,$lat) {
-        
-        $req = "INSERT INTO basev ( lon , lat ) VALUES ( '" . $Lon . "','" . $Lat . "')";
+    function Insert($geo) {
+        //$geo = new GeolocalisationEntity();
+        $req = "INSERT INTO geolocalisation ( lon , lat ) VALUES ( '" . $geo->Lon() . "','" . $geo->Lat() . "')";
         mysql_query($req) or die("********** Erreur d'ajoute **********<br>");
         echo "********** Ajout avec succés **********<br>";
     }
 
+    function delete($geo) {
+        //$geo = new GeolocalisationEntity();
+        $req = "DELETE FROM geolocalisation WHERE lon =  '" . $geo->getLon() . "' and lat = '" . $geo->getLat() . "'";
+        mysql_query($req) or die("********** Erreur de suprission **********<br>");
+        echo "********** Supprission avec succés **********<br>";
+    }
+
+    function update($geo) {
+        //$geo = new GeolocalisationEntity();
+        $req = "UPDATE geolocalisation SET lon ='" . $geo->getLon() . "', lat ='" . $geo->getLon() . "' WHERE id ='" . $geo->getId() . "'";
+        mysql_query($req) or die("********** Erreur de mise à jour **********<br>");
+        echo "********** Mise à jour avec succés **********<br>";
+    }
+
+    function AfficheAllGeo() {
+
+        $result = mysql_query("SELECT * FROM geolocalisation");
+        $list[] = array();
+
+        while ($row = mysql_fetch_array($result)) {
+            $geo = new GeolocalisationEntity();
+            $geo->setId($row['id']);
+            $geo->setLat($row['lat']);
+            $geo->setLon($row['lon']);
+            $list[] = $geo;
+        }
+        return $list;
+    }
+
 }
+
+$dao = new GeolocalisationDAO();
+$entity = new GeolocalisationEntity();
+$list = $dao->AfficheAllGeo();
+
+foreach ($list as $entity) {
+    echo "lat:" . $entity->getLat() . " lon:" . $entity->getLon()."<br>";
+}
+
