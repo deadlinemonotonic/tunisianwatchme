e5496a272824834cc54f76fdf2ed40716f347938
DELL <DELL@MEKNI_Aymen>
2014-04-04 19:38:07 +0200

    Merge origin/master
    Conflicts:
    	dao/utilisateur_dao.php

--- connection/connection.php
+++ connection/connection.php
@@ -1,10 +1,26 @@
 <?php
-$username = "root";
-$password = "root";
-$hostname = "localhost"; 
-$bd= "tunisianwatch";
-        
-$dbhandle = mysql_connect($hostname, $username, $password) or die("Unable to connect to MySQL");
 
-$selectDB = mysql_select_db($bd,$dbhandle) or die("Could not select the database");
+class connection {
+
+    private $username;
+    private $password;
+    private $hostname;
+    private $bd;
+
+    public function __construct() {
+        $this->bd = "tunisianwatch";
+        $this->hostname = "root";
+        $this->password = "root";
+        $this->username = "localhost";
+        
+    }
+    
+    public function connection(){
+        $dbhandle = mysql_connect($this->hostname, $this->username, $this->password) or die("Unable to connect to MySQL");
+        $selectDB = mysql_select_db($this->bd, $dbhandle) or die("Could not select the database");
+        return $selectDB;
+    }
+
+}
+
 ?>
