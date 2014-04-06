0003a678da9b681553d9e4165be0afca0a76966e
Khadija <Khadija@Khadija-PC.esprit.tn>
2014-04-06 20:04:03 +0100

    Merge origin/master
    Conflicts:
    	connection/connection.php
    	dao/Evaluation_DAO.php
    	dao/Geolocalisation_DAO.php
    	dao/commentaire_dao.php
    	dao/domaine_dao.php
    	dao/lieu_dao.php
    	dao/reclamation_dao.php
    	dao/utilisateur_dao.php
    	entity/CommentaireEntity.php
    	entity/UtilisateurEntity.php
    	parsing/getXmlUtilisateur.php
    	parsing/reclamation_xmlparser.php
    	reclamations.php

--- parsing/commentaire_xmlparser.php
+++ parsing/commentaire_xmlparser.php
@@ -5,41 +5,47 @@ include_once 'connection/connection.php';
 
 class CommentaireXMLParser {
 
-
     function __construct($id) {
         $dao = new commentaire_dao();
 
-            $list = $dao->getByidReclamation($id);
-        
+        $list = $dao->getByidReclamation($id);
+
         $this->setXML($list);
     }
 
     function setXML($listCommentaires) {
         header('Content-type: text/xml; charset=UTF-8');
-        //$commentaire = new CommentaireEntity();
+//        $commentaire = new CommentaireEntity();
         $oXMLWriter = new XMLWriter;
         $oXMLWriter->openMemory();
-        $oXMLWriter->startDocument('1.0', 'UTF-8');       
+        $oXMLWriter->startDocument('1.0', 'UTF-8');
         $oXMLWriter->startElement('commentaires');
-            foreach($listCommentaires as $commentaire){
+        foreach ($listCommentaires as $commentaire) {
             $oXMLWriter->startElement('commentaire');
-                   $oXMLWriter->startElement('id');
-                        $oXMLWriter->text($commentaire->getId());
-                   $oXMLWriter->endElement();
-                   $oXMLWriter->startElement('texte');
-                        $oXMLWriter->text($commentaire->getTexte());
-                   $oXMLWriter->endElement();
-                   $oXMLWriter->startElement('idutilisateur');
-                        $oXMLWriter->text($commentaire->getUser());
-                   $oXMLWriter->endElement();
-                   $oXMLWriter->startElement('idreclamation');
-                        $oXMLWriter->text($commentaire->getReclamation());
-                   $oXMLWriter->endElement();
-                   $oXMLWriter->startElement('date');
-                        $oXMLWriter->text($commentaire->getDate());
-                   $oXMLWriter->endElement();
+            $oXMLWriter->startElement('id');
+            $oXMLWriter->text($commentaire->getId());
             $oXMLWriter->endElement();
+            $oXMLWriter->startElement('texte');
+            $oXMLWriter->text($commentaire->getTexte());
+            $oXMLWriter->endElement();
+            $oXMLWriter->startElement('utilisateur');
+            if ($commentaire->getUser() != "") {
+                $oXMLWriter->startElement('nom');
+                $oXMLWriter->text($commentaire->getUser()->getNom());
+                $oXMLWriter->endElement();
+                $oXMLWriter->startElement('prenom');
+                $oXMLWriter->text($commentaire->getUser()->getPrenom());
+                $oXMLWriter->endElement();
             }
+            $oXMLWriter->endElement();
+            $oXMLWriter->startElement('idreclamation');
+            $oXMLWriter->text($commentaire->getReclamation());
+            $oXMLWriter->endElement();
+            $oXMLWriter->startElement('date');
+            $oXMLWriter->text($commentaire->getDate());
+            $oXMLWriter->endElement();
+            $oXMLWriter->endElement();
+        }
         $oXMLWriter->endElement();
         $oXMLWriter->endDocument();
         echo $oXMLWriter->outputMemory(TRUE);
