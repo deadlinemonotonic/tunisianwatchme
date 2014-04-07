bb40c8ee9ddc0142336b3149d851ee444b810538
asd <asd@asd-PC>
2014-04-07 21:00:47 +0200

    Merge origin/master
    Conflicts:
    	tunisianwatchme/src/com/tunisianwatchme/Gui/Midlet.java
    	tunisianwatchme/src/com/tunisianwatchme/Gui/testLieuHundler.java
    	tunisianwatchme/src/com/tunisianwatchme/Handler/DomaineHandler.java
    	tunisianwatchme/src/com/tunisianwatchme/Handler/LieuHandler.java

--- geolocalisations.php
+++ geolocalisations.php
@@ -1,12 +1,25 @@
 <?php
 
-/* 
+/*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
 
 include_once 'parsing/geolocalisationXmlparser.php';
-if(isset($_GET["id"])){
-    $c = new GeolocalisationXmlparser($_GET["id"]);
+if (isset($_GET["type"])) {
+    if ($_GET["type"] == "select") {
+        if (isset($_GET["id"])) {
+            $c = new GeolocalisationXmlparser($_GET["id"]);
+        }
+    }
+    else if($_GET["type"]=="add"){
+        if(isset($_GET["lon"]) && isset($_GET["lat"])){
+            $geolocalisation = new GeolocalisationEntity();
+            $geolocalisation->setLat($_GET["lat"]);
+            $geolocalisation->setLon($_GET["lon"]);
+            $geoDao = new GeolocalisationDAO();
+            $geoDao->Insert($geolocalisation);
+        }
+    }
 }
